84b122d1f6b2e2a3a70c8ff5103b38b48bed4494
Hoang Vu Hung <hoanghung250604@gmail.com>
2023-05-12 09:48:20 +0700

    bug at xepLoai()

--- main.js
+++ main.js
@@ -1,44 +1,96 @@
+var index = 1;
 
-alert("Chào mừng bạn đến với ứng dụng JavaScript đầu tiên");
+function addRow() {
 
-//      Lấy thông tin từ người dùng
-var ten = prompt("Nhập tên của bạn:");
-var thanhPho = prompt("Nhập thành phố bạn sống:");
-var sdt = prompt("Nhập số điện thoại:");
+    var tbody = document.getElementById("tbody"); // biến tbody dùng để truyền nội dung vào thẻ html id body
+    var trAo = document.createElement("tr");// biến trAo dùng để tạo 1 hàng mới
 
-//      In kết quả lên màn console
-console.log(ten);
-console.log(thanhPho);
-console.log(sdt);
+    tbody.append(trAo); // chèn nội dung biến trAo vào biến tbody
+    trAo.id = index;
 
-//      Gán giá trị nhận được vào phần tử HTML
-var nameAssign = document.getElementById("name");
-var cityAssign = document.getElementById("city");
-var mobileAssign = document.getElementById("mobile"); 
+    //  tạo hàng, truyền nội dung biến vào các ô
+    var tdHoTen = document.createElement("td");
+    var tdNgaySinh = document.createElement("td");
+    var tdDiemToan = document.createElement("td");
+    var tdDiemLy = document.createElement("td");
+    var tdDiemHoa = document.createElement("td");
+    var tdDiemTrungBinh = document.createElement("td");
+    var tdHocLuc = document.createElement("td");
 
-//      In kết quả ra màn hình
-nameAssign.innerHTML = ten;
-cityAssign.innerHTML = thanhPho;
-mobileAssign.innerHTML = sdt;
+    //      id các ô sẽ được tăng dần theo từng hàng
+    tdDiemToan.id = "diemToan" + index;
+    tdDiemLy.id = "diemLy" + index;
+    tdDiemHoa.id = "diemHoa" + index;
+    tdDiemTrungBinh.id = "diemTrungBinh" + index;
+    tdHocLuc.id = "hocLuc" + index;
 
-//       Vòng lặp yêu cầu người dùng nhập ngày sinh hợp lệ
-var yeuCauNhapTuoi = true;
-while(yeuCauNhapTuoi){
-    var namSinh = prompt("Nhập năm sinh:");
-    var ageAssign = document.getElementById("age");
-    var now = new Date().getFullYear();
-    ageAssign.innerHTML = "Tuổi của bạn là: " + (now - namSinh);
-    if(namSinh > now){
-         console.log('xạo vậy ba!');
-    } else{
-        yeuCauNhapTuoi = false;
+    index = index + 1;
+
+    //      chèn nội dung vào trAo
+    trAo.append(tdHoTen);
+    trAo.append(tdNgaySinh);
+    trAo.append(tdDiemToan);
+    trAo.append(tdDiemLy);
+    trAo.append(tdDiemHoa);
+    trAo.append(tdDiemTrungBinh);
+    trAo.append(tdHocLuc);
+
+    //  lấy giá trị từ input bằng value 
+    //  và truyền vào các biến td rồi in ra màn hình
+    tdHoTen.innerHTML = nhapTen.value;
+    tdNgaySinh.innerHTML = nhapNamSinh.value;
+    tdDiemToan.innerHTML = nhapDiemToan.value;
+    tdDiemLy.innerHTML = nhapDiemLy.value;
+    tdDiemHoa.innerHTML = nhapDiemHoa.value;
+
+
+}
+
+function diemTB() {
+    //      vòng lặp chạy dựa theo id các ô 
+    for (var i = 1; i < index; i++) {
+        //  các biến sẽ truyền giá trị vào các thẻ có id tương úng
+        var dToan = document.getElementById("diemToan" + i);
+        var dLy = document.getElementById("diemLy" + i);
+        var dHoa = document.getElementById("diemHoa" + i);
+        var dTB = document.getElementById("diemTrungBinh" + i);
+
+        console.log(dToan.innerHTML);
+        console.log(dLy.innerHTML);
+        console.log(dHoa.innerHTML);
+
+        //  lúc này kiểu dữ liệu các biến khi in ra màn hình đang là string
+        //  dùng 2 lần dấu - để chuyển string thành kiểu number
+        dTB.innerHTML = (dToan.innerHTML - (-dLy.innerHTML) - (-dHoa.innerHTML)) / 3;
+        console.log(dTB.innerHTML);
     }
 }
 
-//      Hiển thị nội dung sau khi bấm nút
-var messageAssign = document.getElementById("message");
-document.getElementById("hien").onclick = hienAn;
-function hienAn() {
-    console.log("đã bấm nút");
-    message.style = "display: block";
+function xepLoai() {
+    // truyền nội dung biến vào các ô có id tương ứng
+    var tB = document.getElementById("diemTrungBinh1");
+    var hLuc = document.getElementById("hocLuc1");
+
+    // rẽ nhánh phân loại học lực
+   if (tB > 9 && tB <= 10) {
+        hLuc.innerHTML = "Xuất sắc";
+    } else if (tB > 8 && tB <= 9) {
+        hLuc.innerHTML = "Giỏi";
+    } else if (tB > 7 && tB <= 8) {
+        hLuc.innerHTML = "Khá";
+    } else if (tB > 5 && tB <= 7) {
+        hLuc.innerHTML = "Trung bình"; 
+    } else if (tB > 4 && tB <= 5) {
+        hLuc.innerHTML = "Yếu"; 
+    } else if (tB > 0 && tB <= 4) {
+        hLuc.innerHTML = "Kém"; 
+    }  
+
 }
+
+
+
+
+
+
+
