7bee73312c29df54ff05e7e30dbaea7670325ea5
Hoang Vu Hung <hoanghung250604@gmail.com>
2023-05-30 14:59:31 +0700

    asm4 check

---
+++ index.js
@@ -0,0 +1,297 @@
+
+var staffs = [
+  {
+    "id": 0,
+    "name": "Nguyễn Văn A",
+    "doB": "1999-01-01T08:59:00.000Z",
+    "salaryScale": 1.1,
+    "startDate": "2019-04-30T08:59:00.000Z",
+    "departmentId": "Dept01",
+    "annualLeave": 1,
+    "overTime": 1,
+    "image": "https://cdn.pixabay.com/photo/2016/03/31/19/10/avatar-1294775_1280.png"
+  },
+
+  {
+    "id": 1,
+    "name": "Nguyễn Văn B",
+    "doB": "2000-01-01T08:59:00.000Z",
+    "salaryScale": 1.2,
+    "startDate": "2019-04-30T08:59:00.000Z",
+    "departmentId": "Dept02",
+    "annualLeave": 2,
+    "overTime": 3,
+    "image": "https://cdn.pixabay.com/photo/2016/11/14/17/39/person-1824147_640.png"
+  },
+
+  {
+    "id": 2,
+    "name": "Nguyễn Văn C",
+    "doB": "2001-01-01T08:59:00.000Z",
+    "salaryScale": 1,
+    "startDate": "2019-04-30T08:59:00.000Z",
+    "departmentId": "Dept04",
+    "annualLeave": 4,
+    "overTime": 5,
+    "image": "https://cdn.pixabay.com/photo/2016/11/14/17/39/person-1824147_640.png"
+  },
+
+  {
+    "id": 3,
+    "name": "Nguyễn Văn D",
+    "doB": "2002-01-01T08:59:00.000Z",
+    "salaryScale": 1,
+    "startDate": "2019-04-30T08:59:00.000Z",
+    "departmentId": "Dept03",
+    "annualLeave": 6,
+    "overTime": 7,
+    "image": "https://cdn.pixabay.com/photo/2016/11/14/17/39/person-1824147_640.png"
+  },
+
+  {
+    "id": 4,
+    "name": "Nguyễn Văn E",
+    "doB": "1999-01-01T08:59:00.000Z",
+    "salaryScale": 1,
+    "startDate": "2019-04-30T08:59:00.000Z",
+    "departmentId": "Dept03",
+    "annualLeave": 8,
+    "overTime": 1,
+    "image": "https://cdn.pixabay.com/photo/2016/11/14/17/39/person-1824147_640.png"
+  },
+
+  {
+    "id": 5,
+    "name": "Nguyễn Văn F",
+    "doB": "2003-01-01T08:59:00.000Z",
+    "salaryScale": 1,
+    "startDate": "2019-04-30T08:59:00.000Z",
+    "departmentId": "Dept05",
+    "annualLeave": 9,
+    "overTime": 10,
+    "image": "https://cdn.pixabay.com/photo/2016/11/14/17/39/person-1824147_640.png"
+  },
+
+  {
+    "id": 6,
+    "name": "Nguyễn Văn G",
+    "doB": "2003-01-05T08:59:00.000Z",
+    "salaryScale": 1,
+    "startDate": "2017-04-28T08:59:00.000Z",
+    "departmentId": "Dept05",
+    "annualLeave": 9,
+    "overTime": 10,
+    "image": "https://cdn.pixabay.com/photo/2016/11/14/17/39/person-1824147_640.png"
+  },
+
+  {
+    "id": 7,
+    "name": "Nguyễn Văn H",
+    "doB": "2003-01-01T08:59:00.000Z",
+    "salaryScale": 1,
+    "startDate": "2019-04-30T08:59:00.000Z",
+    "departmentId": "Dept03",
+    "annualLeave": 9,
+    "overTime": 10,
+    "image": "https://cdn.pixabay.com/photo/2016/11/14/17/39/person-1824147_640.png"
+  },
+
+  {
+    "id": 8,
+    "name": "Nguyễn Văn I",
+    "doB": "2003-01-01T08:59:00.000Z",
+    "salaryScale": 1,
+    "startDate": "2019-04-30T08:59:00.000Z",
+    "departmentId": "Dept04",
+    "annualLeave": 9,
+    "overTime": 10,
+    "image": "https://cdn.pixabay.com/photo/2016/11/14/17/39/person-1824147_640.png"
+  },
+
+  {
+    "id": 9,
+    "name": "Nguyễn Văn L",
+    "doB": "2002-01-01T08:59:00.000Z",
+    "salaryScale": 1,
+    "startDate": "2019-04-30T08:59:00.000Z",
+    "departmentId": "Dept05",
+    "annualLeave": 9,
+    "overTime": 10,
+    "image": "https://cdn.pixabay.com/photo/2016/11/14/17/39/person-1824147_640.png"
+  },
+
+  {
+    "id": 10,
+    "name": "Nguyễn Văn M",
+    "doB": "2003-01-01T08:59:00.000Z",
+    "salaryScale": 1,
+    "startDate": "2019-04-30T08:59:00.000Z",
+    "departmentId": "Dept05",
+    "annualLeave": 9,
+    "overTime": 10,
+    "image": "https://cdn.pixabay.com/photo/2016/11/14/17/39/person-1824147_640.png"
+  },
+
+  {
+    "id": 11,
+    "name": "Nguyễn Văn N",
+    "doB": "2003-01-01T08:59:00.000Z",
+    "salaryScale": 1,
+    "startDate": "2019-04-30T08:59:00.000Z",
+    "departmentId": "Dept05",
+    "annualLeave": 9,
+    "overTime": 10,
+    "image": "https://cdn.pixabay.com/photo/2016/11/14/17/39/person-1824147_640.png"
+  },
+
+  {
+    "id": 12,
+    "name": "Nguyễn Văn O",
+    "doB": "2003-01-01T08:59:00.000Z",
+    "salaryScale": 1,
+    "startDate": "2019-04-30T08:59:00.000Z",
+    "departmentId": "Dept05",
+    "annualLeave": 9,
+    "overTime": 10,
+    "image": "https://cdn.pixabay.com/photo/2016/11/14/17/39/person-1824147_640.png"
+  },
+
+  {
+    "id": 13,
+    "name": "Nguyễn Văn S",
+    "doB": "2003-01-01T08:59:00.000Z",
+    "salaryScale": 1,
+    "startDate": "2019-04-30T08:59:00.000Z",
+    "departmentId": "Dept05",
+    "annualLeave": 9,
+    "overTime": 10,
+    "image": "https://cdn.pixabay.com/photo/2016/11/14/17/39/person-1824147_640.png"
+  },
+
+  {
+    "id": 14,
+    "name": "Nguyễn Văn R",
+    "doB": "2003-01-01T08:59:00.000Z",
+    "salaryScale": 1,
+    "startDate": "2019-04-30T08:59:00.000Z",
+    "departmentId": "Dept05",
+    "annualLeave": 9,
+    "overTime": 10,
+    "image": "https://cdn.pixabay.com/photo/2016/11/14/17/39/person-1824147_640.png"
+  },
+
+  {
+    "id": 15,
+    "name": "Nguyễn Văn T",
+    "doB": "2001-01-01T08:59:00.000Z",
+    "salaryScale": 1,
+    "startDate": "2019-04-30T08:59:00.000Z",
+    "departmentId": "Dept05",
+    "annualLeave": 9,
+    "overTime": 10,
+    "image": "https://cdn.pixabay.com/photo/2016/11/14/17/39/person-1824147_640.png"
+  }];
+
+var staffsAPI = "http://localhost:8080/staffs"; 
+
+function staffsList() {
+  fetch(staffsAPI)
+  .then(function (response) {
+    return response.json();
+  }).then(function(staffs) {
+    // html code created from staffs list
+    let staffsDiv = $("#staffs");
+    
+    staffs.forEach((staff) => {
+      console.log(staff);
+      let staffDivs = $("<div></div>").attr("id", "staff" + staff.id);
+      
+      let staffImage = $("<img>").attr("src", staff.image);
+      let staffName = $("<p></p>").attr("id", "staffName" + staff.id).html(staff.name);
+      let staffId = $("<p></p>").html("ID: " + staff.id);
+      let detailBtn = $("<button>Detail</button>");
+      let dltBtn = $("<button>Delete</button>").attr("id", staff.id).click(function (event){
+        console.log(event.target.id);
+        //delete staff by id
+        fetch(staffsAPI + "/" + event.target.id, {
+          method: "DELETE",
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+        
+        })
+          .then((response) => {
+            console.log(response);
+          });
+      })
+
+      staffDivs.append(staffImage);
+      staffDivs.append(staffName);
+      staffDivs.append(staffId);
+      staffDivs.append(detailBtn);
+      staffDivs.append(dltBtn);
+
+      staffsDiv.append(staffDivs);
+    })
+  })
+  //warning api error
+  .catch((error) => {
+    alert(error);
+  });
+}
+
+//
+    var addBtn = document.querySelector("#add");
+    addBtn.onclick = function(){
+      var name = document.querySelector('input[name="name"]').value;
+      var doB = document.querySelector('input[name="doB"]').value;
+      var departmentId = document.querySelector('input[name="departmentId"]').value;
+
+      var formData = {
+        name: name,
+        doB: doB,
+        departmentId: departmentId,
+        image: "https://cdn.pixabay.com/photo/2016/11/14/17/39/person-1824147_640.png"
+      };  
+      //get data from form then put into addStaff() 
+      addStaff(formData); 
+    }
+
+function addStaff(formData) {
+  fetch(staffsAPI, {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+    .then((response) => {
+      console.log(response);
+    });
+}
+
+// function update(formData) {
+//   fetch(staffsAPI + "/" +   {
+//   method: "PATCH", 
+//   headers: {
+//   'Accept': 'application/json',
+//   'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify(formData);
+  
+//   }).then((response) => {
+//   console.log(response);
+//   });
+//   }
+
+
+ 
+
+
+
+
+
+
+
